71b09ae71dbb003fd7af081935e4d8674048f79b
谦哥 <qqyule@gmail.com>
2022-07-29 19:04:35 +0800

    ha

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,6 +4,6 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1659019041142_6702',
   koa: {
-    port: 7001,
+    port: 80,
   },
 } as MidwayConfig;
